9d9a1c008903298ca20b32de8106f04010f6f8f6
Neeraj jeena <neeraj2005jeena@gmail.com>
2026-01-20 21:06:12 +0530

    done

--- shadowgurd-security/src/App.jsx
+++ shadowgurd-security/src/App.jsx
@@ -1,30 +1,4 @@
-// import { HashRouter, Routes, Route } from "react-router-dom";
 
-// import Navbar from "./components/Navbar";
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import Services from "./components/Services";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-
-// function App() {
-//   return (
-//     <HashRouter>
-//       <Navbar />
-
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/services" element={<Services />} />
-//         <Route path="/contact" element={<Contact />} />
-//       </Routes>
-
-//       <Footer />
-//     </HashRouter>
-//   );
-// }
-
-// export default App;
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
